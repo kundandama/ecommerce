68be7ca328547650f7213830c785ed77ba1917f2
Kundan <kundan@Kundans-MacBook-Air.local>
2025-05-20 12:53:50 +0530

    fixed issues

--- ecommerce/src/pages/CartPage/CartPage.jsx
+++ ecommerce/src/pages/CartPage/CartPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Cart from "../../components/Cart";
 function CartPage() {
-  return (
-  //   <div>
-  //     <Cart />
-  //   </div>
+  // return (
+  //   // <div>
+  //   //   {/* <Cart /> */}
+  //   // </div>
   // );
 }
 export default CartPage;
